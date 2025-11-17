365bf4efb5016cf5e20a5af810c9541834b3b477
taharizwan1100-lang <taharizwan1100@gmail.com>
2025-11-18 01:54:04 +0500

    Add custom colors to Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'dark-bg': '#0D081E', // A deep, dark purple/indigo
+        'primary-purple': '#8A2BE2', // A vibrant purple for accents
+        // You'll need to define your glow colors here if you use custom utility classes
+      },
+    },
+  },
+  // ... other configs
+}
